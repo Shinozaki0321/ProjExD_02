02165dfc8814ff00ef00ff3529b18360efc03915
shinozaki <c0a22078c7@edu.teu.ac.jp>
2023-04-25 18:20:12 +0900

    Isuue修正

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import pygame as pg
-import sys
 import random
+import sys
+
 
 delta = {
         pg.K_UP: (0, -1),
@@ -41,7 +42,7 @@ def main():
     # screen.blit(bb_img, [x, y])
     vx, vy = +1, +1
     bb_rct = bb_img.get_rect()
-    bb_rct.center
+    bb_rct.center = x, y
 
     tmr = 0
     
@@ -71,9 +72,8 @@ def main():
             vy *= -1
         screen.blit(bb_img, bb_rct)
         if kk_rct.colliderect(bb_rct):
-            kk_img = pg.image.load("ex02/fig/8.png")
-            kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-            screen.blit(kk_img, kk_rct)
+            kk_img = pg.transform.rotozoom(pg.image.load("ex02/fig/8.png"), 0, 2.0)
+            clock = pg.time.Clock()
             clock.tick(0.5)
             return
 
