e16be36b3d4cf321bf7fc4dc46ad4bfd4fe62fac
shinozaki <c0a22078c7@edu.teu.ac.jp>
2023-04-25 14:36:37 +0900

    練習問題３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,10 +14,12 @@ def main():
     pg.draw.circle(bb_img,(255, 0, 0), (10, 10), 10)  #赤い円
     bb_img.set_colorkey((0, 0, 0))  # 四隅を透過
     x, y = random.randint(0, 1600), random.randint(0, 900)
-    screen.blit(bb_img, [x, y])
+    # screen.blit(bb_img, [x, y])
+    vx, vy = +1, +1
+    bb_rct = bb_img.get_rect()
+    bb_rct.center
 
     tmr = 0
-
     
     while True:
         for event in pg.event.get():
@@ -27,7 +29,8 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [x, y])
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
 
         pg.display.update()
         clock.tick(1000)
