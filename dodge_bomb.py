b65a937c03a6a3a9145953148113637e8f5e23d8
shinozaki <c0a22078c7@edu.teu.ac.jp>
2023-04-25 18:22:46 +0900

    追加機能３（未完）

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -71,6 +71,7 @@ def main():
         if not tate:
             vy *= -1
         screen.blit(bb_img, bb_rct)
+        
         if kk_rct.colliderect(bb_rct):
             kk_img = pg.transform.rotozoom(pg.image.load("ex02/fig/8.png"), 0, 2.0)
             clock = pg.time.Clock()
